21ff15688d73b36b7afac096c1fac946c9d53517
pavithrasubbarayan <pavithra.anus50@gmail.com>
2016-07-21 08:06:34 +0530

    alignment

--- java exaample/src/exercise/PrimeNumbers.java
+++ java exaample/src/exercise/PrimeNumbers.java
@@ -2,29 +2,18 @@ package exercise;
 
 public class PrimeNumbers {
 	public static void main(String[] args) {
-
 		int num = 100;
 		int count = 0;
-
 		for (int i = 2; i <= num; i++) {
-
 			count = 0;
-
 			for (int j = 2; j <= i / 2; j++) {
-
 				if (i % j == 0) {
 					count++;
 					break;
 				}
-
 			}
-
-			if (count == 0) {
-
+			if (count == 0)
 				System.out.println(i);
-
-			}
-
 		}
 
 	}

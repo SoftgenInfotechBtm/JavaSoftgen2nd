8f004ae13595f19cd4cb4f4e0c676cdadc232719
pavithrasubbarayan <pavithra.anus50@gmail.com>
2016-07-21 07:52:03 +0530

    alignment of code phase one

--- java exaample/src/exercise/MyString.java
+++ java exaample/src/exercise/MyString.java
@@ -1,15 +1,13 @@
 package exercise;
-    public final class MyString
-	{
-	 final String str;
-	 MyString(String s)
-	 {
-	  this.str = s;
-	 }
-	 public String get()
-	 {
-	  return str;
-	 }
+
+public final class MyString {
+	final String str;
+
+	MyString(String s) {
+		this.str = s;
 	}
 
-
+	public String get() {
+		return str;
+	}
+}
--- java exaample/src/exercise/Swaping.java
+++ java exaample/src/exercise/Swaping.java
@@ -2,27 +2,23 @@ package exercise;
 
 public class Swaping {
 
+	public static void main(String[] args) {
 
-		public static void main(String[] args) {
-		 
-		int number1=20;
-		int number2=30;
-		 
+		int number1 = 20;
+		int number2 = 30;
+
 		System.out.println("Before Swapping");
 		System.out.println("Value of number1 is :" + number1);
-		System.out.println("Value of number2 is :" +number2); 
-		 
-		number1=number1+number2;
-		number2=number1-number2;
-		number1=number1-number2;
-		 
+		System.out.println("Value of number2 is :" + number2);
+
+		number1 = number1 + number2;
+		number2 = number1 - number2;
+		number1 = number1 - number2;
+
 		System.out.println("After Swapping");
 		System.out.println("Value of number1 is :" + number1);
-		System.out.println("Value of number2 is :" +number2);
-		 
-		}
-		
-    }
-   
-    
+		System.out.println("Value of number2 is :" + number2);
 
+	}
+
+}

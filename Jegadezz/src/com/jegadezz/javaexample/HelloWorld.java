eeac0902282a505642dca99588dfbfb074e7bd2a
JegadezzIn <jegadezz.freelancer@gmail.com>
2016-09-25 08:06:44 +0530

    jegadezz new java project

---
+++ Jegadezz/src/com/jegadezz/javaexample/HelloWorld.java
@@ -0,0 +1,5 @@
+package com.jegadezz.javaexample;
+
+public class HelloWorld {
+
+}
---
+++ Jegadezz/src/jegadezz.java
@@ -0,0 +1,6 @@
+
+public class jegadezz {
+		public static void main(String[] args) {
+			System.out.println("jegadezz");
+		}
+}

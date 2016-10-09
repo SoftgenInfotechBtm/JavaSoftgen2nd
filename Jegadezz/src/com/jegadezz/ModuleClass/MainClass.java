082fad092c727d1a247be7f51c8ff2e269775a53
JegadezzIn <jegadezz.freelancer@gmail.com>
2016-10-09 07:24:32 +0530

    Encapsulation
    Oops concept

---
+++ Jegadezz/src/com/jegadezz/ModuleClass/MainClass.java
@@ -0,0 +1,11 @@
+package com.jegadezz.ModuleClass;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Jegadezz obj1 = new Jegadezz();
+		obj1.setEmpid(7876);
+		obj1.setName("jegadezz");
+		System.out.println("Employee name " + obj1.getName());
+		System.out.println("Employee ID" + obj1.getEmpid());
+	}
+}
